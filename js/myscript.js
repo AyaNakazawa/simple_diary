2f38617e4911112ae821210bb3a2bc346c0814e7
Aya Nakazawa <aya@ayatec.top>
2017-04-30 16:36:57 +0900

    Update code to ES2015

--- js/myscript.js
+++ js/myscript.js
@@ -16,47 +16,26 @@ $(function() {
 // Functions
 
 // ----------------------------------------------------------------
-// Dateオブジェクトからゼロ埋めした日付文字列を生成
-// _date: new Date()
-//  Dateオブジェクト
-// _format: '%Y/%m/%d %H:%M:%S'
-//  %Y: 年4桁
-//  %y: 年2桁
-//  %m: 月
-//  %d: 日
-//  %H: 時
-//  %M: 分
-//  %S: 秒
-function getDateString(_date, _format){
-  var date = _date || new Date();
-  var dateString = _format || '%Y/%m/%d %H:%M:%S';
-  
-  if (dateString.indexOf('%Y') >= 0) {
-    dateString = dateString.replace('%Y', ("000" + _date.getFullYear()).slice(-4));
-  }
-  if (dateString.indexOf('%y') >= 0) {
-    dateString = dateString.replace('%y', ("0" + _date.getFullYear()).slice(-2));
-  }
-  if (dateString.indexOf('%m') >= 0) {
-    dateString = dateString.replace('%m', ("0" + (_date.getMonth() + 1)).slice(-2));
-  }
-  if (dateString.indexOf('%d') >= 0) {
-    dateString = dateString.replace('%d', ("0" + _date.getDate()).slice(-2));
-  }
-  if (dateString.indexOf('%H') >= 0) {
-    dateString = dateString.replace('%H', ("0" + _date.getHours()).slice(-2));
-  }
-  if (dateString.indexOf('%M') >= 0) {
-    dateString = dateString.replace('%M', ("0" + _date.getMinutes()).slice(-2));
-  }
-  if (dateString.indexOf('%S') >= 0) {
-    dateString = dateString.replace('%S', ("0" + _date.getSeconds()).slice(-2));
-  }
-  return dateString;
-}
+// Classes
 
-// ----------------------------------------------------------------
-// true false をスイッチ
-function toggleBoolean(_bool) {
-  return !_bool;
+class DatePlus extends Date {
+  getString(format = '%Y/%m/%d %H:%M:%S') {
+    // Dateオブジェクトからゼロ埋めした日付文字列を生成
+    // format: '%Y/%m/%d %H:%M:%S'
+    //  %Y: 年4桁
+    //  %y: 年2桁
+    //  %m: 月
+    //  %d: 日
+    //  %H: 時
+    //  %M: 分
+    //  %S: 秒
+    format = format.replace('%Y', ("000" + this.getFullYear()).slice(-4));
+    format = format.replace('%y', ("0" + this.getFullYear()).slice(-2));
+    format = format.replace('%m', ("0" + (this.getMonth() + 1)).slice(-2));
+    format = format.replace('%d', ("0" + this.getDate()).slice(-2));
+    format = format.replace('%H', ("0" + this.getHours()).slice(-2));
+    format = format.replace('%M', ("0" + this.getMinutes()).slice(-2));
+    format = format.replace('%S', ("0" + this.getSeconds()).slice(-2));
+    return format;
+  }
 }
